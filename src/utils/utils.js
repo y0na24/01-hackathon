febc1c554084e3ca1838f6baa37ad37f28498d57
y0na24 <y0na24@mail.ru>
2023-01-20 21:07:22 +0300

    changed font, point events and method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,12 @@
 export const addHideClass = ((targetPath) => {
+
   if (!targetPath) {
     throw new Error(`Target path should not be empty`)
   }
-  if (!document.querySelector(targetPath)?.classList?.contains('hide')) {
-    const elementFromUI = document.querySelector(targetPath)
-    elementFromUI?.classList?.add('hide')
+
+  const imgUI = document.querySelector(targetPath)
+
+  if (imgUI && !imgUI.classList.contains('hide')) {
+    imgUI.classList.add('hide')
   }
 })
