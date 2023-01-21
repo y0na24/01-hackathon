b13651f839516ae93f61dd2a6c93749b2ddab19e
Малыхин Илья Андреевич <IMalykhin@ibs.root.ibscorp.ru>
2023-01-21 05:17:58 +0500

    add tree and file feature/background.module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,32 @@
 import {Module} from '../core/module'
+import {random} from '../utils'
 
 export class BackgroundModule extends Module {
+    constructor() {
+        super('background', 'Поменять цвет фона') 
+    }
 
+    trigger() {
+        this.#setBackground();
+    }
+
+    #setBackground() {
+        const directions = ['to right', 'to left', 'to bottom', 'to top', 'to bottom right', 'to bottom left', 'to top right', 'to top left'];
+        if (random(1, 100) < 50) {
+            document.body.style.background = `linear-gradient(${directions[random(0, 7)]}, ${this.#getRandomColor()}, ${this.#getRandomColor()})`;
+            document.body.style.backgroundRepeat = 'no-repeat';
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.height = '100vh';
+
+        } else {
+            document.body.style.background = this.#getRandomColor();
+        }
+    }
+
+    #getRandomColor() {
+      return `rgb(${random(1, 255)}, ${random(1, 255)}, ${random(1, 255)})`;
+    }
 }
+
+
+
