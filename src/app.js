5f07fa6675677af6fed03127bae8b787a7a0929b
herou3 <herou3@yandex.ru>
2023-01-22 13:11:28 +0700

    -Created base code for shape-module, fixed bugs for the click-module, added event for module in context-menu

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import './styles.css'
 import { ContextMenu } from './menu'
 import { BackgroundModule } from './modules/background.module'
 import { ClicksModule } from './modules/clicks.module'
+import { ShapeModule } from './modules/shape.module'
 
 class App{
   #contextMenu
   #backgroundModule
-  #clicksModule 
+  #clicksModule
+  #snapeModule
 
   constructor() {
     this.#backgroundModule = new BackgroundModule('background', 'Change color')
     this.#clicksModule = new ClicksModule("ClicksModule", "Click", document.body, document.body, 5)
-    this.#contextMenu = new ContextMenu('.menu', [this.#backgroundModule, this.#clicksModule])
+    this.#snapeModule = new ShapeModule('SnapeModule', 'Create figure', document.body)
+    this.#contextMenu = new ContextMenu('.menu', [this.#backgroundModule, this.#clicksModule, this.#snapeModule])
   }
 
   run() {
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -2,4 +2,65 @@ import {Module} from '../core/module'
 
 export class ShapeModule extends Module {
 
+  constructor(text, type, parentElementOnTheDOM) {
+    super(type, text)
+    
+    this.parentElementOnTheDOM = parentElementOnTheDOM
+  }
+
+  trigger() {
+    const positionInfo = this.parentElementOnTheDOM.getBoundingClientRect()
+    console.log("height", positionInfo.height)
+    console.log("width", positionInfo.width)
+  }
+
+  #createGeometryFigure(geometryType) {
+    switch (geometryType) {
+      case "square":
+        
+        break;
+      case "rectangle":
+
+        break;
+      default:
+
+        break;
+    }
+  }
+
+
+  #generateSquare(size, xPosition, yPosition, color) {
+    const square = document.createElement('div')
+    square.style.position = "relative"
+    square.style.width = size
+    square.style.height = size
+    square.style.left = xPosition
+    square.style.top = yPosition
+    square.style.background = color
+    return 
+  }
+
+  #generateRectangle(width, height, xPosition, yPosition, color) {
+  
+  }
+
+  #generateCircle(radius, xPosition, yPosition, color) {
+
+  }
+
+  #generateOval(width, height, xPosition, yPosition, color) {
+
+  }
+
+  #generateTriangle(leftIndent, rightIndent, bottomIndent, color) {
+
+  }
+
+  #generateParallelogram(width, height, skew, color) {
+
+  }
+
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+  }
 }
